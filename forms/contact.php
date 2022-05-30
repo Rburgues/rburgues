26076aed2b8ae8f8104ba7fde9268dfe34841b8e
Rburgues <you@example.com>
2022-05-29 23:33:29 -0300

    update antes de mi cumple

--- forms/contact.php
+++ forms/contact.php
@@ -4,9 +4,10 @@
   $email = trim($_POST['email']);
   $nombre = trim($_POST['name']);
   $subject = trim($_POST['subject']);
+  $phone = trim($_POST['telefono']);
   $mensaje = trim($_POST['message']);
   
-  $consulta = "E-mail: " . $email . "Nombre: " . $nombre . "Asunto: " . $subject . "Mensaje: " . $mensaje;
+  $consulta = "E-mail: " . $email . "Nombre: " . $nombre . "Telefono: " . $phone . "Asunto: " . $subject . "Mensaje: " . $mensaje;
   
   mail("contacto@rburgues.com", "Contacto desde la web", $consulta);
   } else {
